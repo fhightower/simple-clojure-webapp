820da3bdf22350f52212211e0d018556f3ee6754
Floyd Hightower <floyd.hightower27@gmail.com>
2021-06-02 16:18:22 -0500

    Creating sample data when starting the app

--- guestbook/env/dev/clj/guestbook/env.clj
+++ guestbook/env/dev/clj/guestbook/env.clj
@@ -2,12 +2,17 @@
   (:require
     [selmer.parser :as parser]
     [clojure.tools.logging :as log]
-    [guestbook.dev-middleware :refer [wrap-dev]]))
+    [guestbook.dev-middleware :refer [wrap-dev]]
+    [guestbook.db.core :as db]))
+
+(defn create-sample-data []
+  (db/create-user! {:id 1 :first_name "Jane" :last_name "Doe" :email "jane@example.com" :pass "abc"}))
 
 (def defaults
   {:init
    (fn []
      (parser/cache-off!)
+     (create-sample-data)
      (log/info "\n-=[guestbook started successfully using the development profile]=-"))
    :stop
    (fn []
